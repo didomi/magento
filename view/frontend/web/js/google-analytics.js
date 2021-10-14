6c53b8050f1116af16012b78284140a0a7fd5def
Paul Negrerie <juanito@dnd.fr>
2021-10-14 10:48:47 +0200

    google-analytics : Fixed google-analytics.js

--- view/frontend/web/js/google-analytics.js
+++ view/frontend/web/js/google-analytics.js
@@ -9,32 +9,30 @@
 /* eslint-disable */
 define([
     'jquery',
-    'mage/cookies'
-], function ($) {
+    'mage/cookies',
+], function($) {
     'use strict';
 
     /**
      * @param {Object} config
      */
-    return function (config) {
+    return function(config) {
+
         var allowServices = false,
             allowedCookies,
             allowedWebsites;
 
         if (config.isDidomiUniversalAnalyticsVendorEnabled) {
             window.didomiOnReady = window.didomiOnReady || [];
-            window.didomiOnReady.push(function (Didomi) {
+            window.didomiOnReady.push(function(Didomi) {
                 if (Didomi.isConsentRequired()) {
                     // Consent is required: your visitor is from the EU or you are an EU company
                     // Only enable the vendor when consent is given
-                    Didomi.getObservableOnUserConsentStatusForVendor(config.didomiUniversalAnalyticsVendorId)
-                    .filter(function(status) { return status === true; }) // Filter out updates where status is not true
-                        .first() // Only get the first consent status update
-                        .subscribe(function (consentStatusForVendor) {
-                            // The user has given consent to the vendor
-                            // Enable it
+                    Didomi.getObservableOnUserConsentStatusForVendor(config.didomiUniversalAnalyticsVendorId).subscribe(function(consentStatus) {
+                        if (consentStatus === true) {
                             allowServices = true;
-                        });
+                        }
+                    });
                 } else {
                     // Consent is not required, enable your vendor immediately
                     allowServices = true;
@@ -55,16 +53,16 @@ define([
         }
 
         if (allowServices) {
-            (function (i, s, o, g, r, a, m) {
+            (function(i, s, o, g, r, a, m) {
                 i.GoogleAnalyticsObject = r;
-                i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments);
                 }, i[r].l = 1 * new Date();
                 a = s.createElement(o),
                     m = s.getElementsByTagName(o)[0];
                 a.async = 1;
                 a.src = g;
-                m.parentNode.insertBefore(a, m)
+                m.parentNode.insertBefore(a, m);
             })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
             // Process page info
@@ -82,14 +80,14 @@ define([
 
                 // Collect product data for GA
                 if (config.ordersTrackingData.products) {
-                    $.each(config.ordersTrackingData.products, function (index, value) {
+                    $.each(config.ordersTrackingData.products, function(index, value) {
                         ga('ec:addProduct', value);
                     });
                 }
 
                 // Collect orders data for GA
                 if (config.ordersTrackingData.orders) {
-                    $.each(config.ordersTrackingData.orders, function (index, value) {
+                    $.each(config.ordersTrackingData.orders, function(index, value) {
                         ga('ec:setAction', 'purchase', value);
                     });
                 }
@@ -100,5 +98,5 @@ define([
                 ga('send', 'pageview' + config.pageTrackingData.optPageUrl);
             }
         }
-    }
+    };
 });
